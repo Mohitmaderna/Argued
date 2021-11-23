b87eb88d0374269918e4c07333fba2d6f32d5d30
Mohitmaderna <mohit.appic@gmail.com>
2021-11-23 19:31:31 +0530

    title & subtitle api integrated
    mobile main-menu is done

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
+
 @Injectable({
     providedIn: 'root'
 })
@@ -8,10 +9,13 @@ export class ApiServices {
 
     API: string;
     httpOptions!: { headers: HttpHeaders; };
+
+
     constructor(private http: HttpClient) {
        this.API = environment.API_URL;
     }
-
+ 
+    // Get Method (Api data)
     get(path: string, params: HttpParams = new HttpParams()) {
         return this.http.get(`${this.API}${path}` , {params})
     }
--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServices } from 'src/app/Services/api.service';
-import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -16,61 +15,38 @@ export class SidenavComponent implements OnInit {
   catagories_id: any;
   profile: any;
   _id: any;
-  newItemEvent = new EventEmitter<string>();
+  loading: boolean = true;
 
   constructor(private apiService: ApiServices) {this.getAllcategories()
   }
 
   ngOnInit(): void {
- 
-  this.getAllcategories()
- 
-  
+    this.getAllcategories()
   }
 
   getAllcategories(){
     this.apiService.get('category').subscribe( (res:any) => {
-      console.log(res);
-      this.catagories = res.data;
-
-      for(let i in res.data){
-        this._id = res.data[i]._id;
-        console.log(this._id);
+      if (res) {
+      this.hideloader();
       }
-      console.log(this._id);
-      //  this.catagories.forEach((element: any) => {
-        
-      //   this.profile = element.subcategories;
-        
-      //   console.log(this.profile)
-      //  });
-      // res.data.forEach((element:any) => {
-      //   if(element.name === '1st Amendment'){
-      //     this.catagories_id = element._id;
-      //   }
-        
-      // });
-    
-      // for(let i in res.data){
-      //   if("5e3aa5719d265a745118f484" === res.data[i]._id){
-      //     console.log(res.data[i]._id)
-      //   }
-      // }
-      this.getAllsubcategories(this._id)
-      console.log(this.catagories)
+      this.catagories = res.data;
+      this.catagories.forEach((element: { _id: any; }) => {
+       if(element._id) {
+         this._id = element._id;
+        }
+      });
     }, err => {
       this.catagories = [];
     });
-    return  this.getAllsubcategories(this._id);
   }
 
   getAllsubcategories(_id: any){
-//  console.log(_id)
     this.apiService.get(`category/${_id}`).subscribe( (res:any)=> {
-      // console.log(_id);
       this.subcategory = res.data.subcategories;
-
-      console.log(this.subcategory);
     });
+  }
+
+  hideloader() {
+    this.loading = false;
   }
 }
